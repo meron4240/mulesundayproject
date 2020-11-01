205b53dba601a2a6fef360b71876dd94c9f33708
meron4240 <meronmaff@gmail.com>
2020-11-02 02:34:42 +0300

    Sunday project two

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Binny is fun");
+        System.out.println("Java is fun");
+        System.out.println("Maths is good");
+    }
+}
