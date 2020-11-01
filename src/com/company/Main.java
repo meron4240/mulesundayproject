320c394aec1453976f7f1c015590468bb25fbc8d
meron4240 <meronmaff@gmail.com>
2020-11-02 02:40:43 +0300

    test case one

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Binny is fun");
         System.out.println("Java is fun");
-        System.out.println("Maths is good");
+        System.out.println("Mule soft project");
+        System.out.println("Meron is ood student");
     }
 }
